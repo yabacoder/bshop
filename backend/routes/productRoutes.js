796e3ddd688a4282007216b617f861f770fe233c
Yabacoder <yabacoder@gmail.com>
2021-02-17 01:17:54 +0100

    custom error

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel';
+import Product from '../models/productModel.js';
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/products', asyncHandler(async (req, res) => {
     const products = await Product.find({});
     
     res.json(products);
 }))
 
 
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get('/product/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id );
 
     if(product) {
         res.json(product);
     } else {
-        res.status(404).json({ message: 'Product notfound'})
+        
+        res.status(404)
+        throw new Error('Product not found');git add .
     } 
 }));
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 
 dotenv.config();
 
@@ -10,10 +11,16 @@ connectDB();
 const app = express();
 
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id == req.params.id);
-  res.json(product);
+app.get('/', (req, res) => {
+ res.send("Api...running")
 });
+
+app.use('/api', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000;
 
