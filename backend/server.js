c721222fed9bcc668f07ddd68acddeeadcc536b3
Yabacoder <yabacoder@gmail.com>
2021-02-19 23:20:22 +0100

    save order

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const express = require ('express');
-const dotenv = require ('dotenv');
-const connectDB = require ('./config/db.js');
-const productRoutes = require ('./routes/productRoutes.js');
-const userRoutes = require ('./routes/userRoutes.js');
-const { notFound, errorHandler } = require ('./middleware/errorMiddleware');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const productRoutes = require('./routes/productRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+const orderRoutes = require('./routes/orderRoutes.js');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
 
@@ -12,19 +13,21 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
- res.send("Api...running")
+	res.send('Api...running');
 });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+	PORT,
+	console.log(`Server Started in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { Link } from 'react-router-dom';
+import { createOrder } from '../actions/orderActions';
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+	const dispatch = useDispatch();
 	const cart = useSelector((state) => state.cart);
 
 	// Calculate prices
@@ -25,7 +27,27 @@ const PlaceOrderScreen = () => {
 		Number(cart.taxPrice)
 	).toFixed(2);
 
-	const placeOrderHandler = () => {};
+	const orderCreate = useSelector((state) => state.orderCreate);
+	const { order, success, error } = orderCreate;
+
+	useEffect(() => {
+		if (success) {
+			history.push(`/order/${order._id}`);
+		}
+	}, [history, success]);
+
+	const placeOrderHandler = () => {
+		dispatch(
+			createOrder({
+				orderItems: cart.cartItems,
+				shippingAddress: cart.shippingAddress,
+				paymentMethod: cart.paymentMethod,
+				itemsPrice: cart.itemsPrice,
+				shippingPrice: cart.shippingPrice,
+				taxPrice: cart.taxPrice,
+			})
+		);
+	};
 
 	return (
 		<>
@@ -101,6 +123,9 @@ const PlaceOrderScreen = () => {
 									<Col>Total</Col>
 									<Col>${cart.totalPrice}</Col>
 								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								{error && <Message variant="danger">{error}</Message>}
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Button
