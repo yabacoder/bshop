4ea637e03fcbc98531cea883a1b8569f6b490eab
Yabacoder <yabacoder@gmail.com>
2021-02-19 23:50:28 +0100

    sauce

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,9 @@ import {
 	ORDER_CREATE_FAIL,
 	ORDER_CREATE_REQUEST,
 	ORDER_CREATE_SUCCESS,
+	ORDER_DETAILS_FAIL,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
 } from '../constants/orderConstants';
 
 const createOrder = (order) => async (dispatch, getState) => {
@@ -40,5 +43,39 @@ const createOrder = (order) => async (dispatch, getState) => {
 		});
 	}
 };
+const orderDetails = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_DETAILS_REQUEST,
+		});
 
-export { createOrder };
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		const { data } = await axios.get(`/api/orders/${id}`, order, config);
+
+		dispatch({
+			type: ORDER_DETAILS_SUCCESS,
+			payload: data,
+		});
+
+		// localStorage.setItem('userInfo', JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: ORDER_DETAILS_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export { createOrder, orderDetails };
